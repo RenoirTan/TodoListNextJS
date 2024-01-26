0aa8d5a8c459a6b47e84f27324fc1427da455215
RenoirTan <renoirtan2005@gmail.com>
2024-01-26 14:30:50 +0800

    main: add some functions

--- lib/data.ts
+++ lib/data.ts
@@ -6,3 +6,11 @@ const prisma = new PrismaClient();
 export async function getRecentTodos() {
   return await prisma.todo.findMany();
 }
+
+export async function createTodo(data: { title: string, description: string, complete: boolean }) {
+  const todo = await prisma.todo.create({ data });
+}
+
+export async function deleteTodo(id: string) {
+  const todo = await prisma.todo.delete({ where: { id } });
+}
