baa63433cc8b36f73e5530aaa9d61cd99be05ffe
RenoirTan <renoirtan2005@gmail.com>
2024-01-27 10:52:55 +0800

    suspense: suspense for edit

--- app/[id]/edit/page.tsx
+++ app/[id]/edit/page.tsx
@@ -1,9 +1,11 @@
 import EditForm from "@/app/ui/edit-form";
-import { getTodo } from "@/lib/actions";
-import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
 export default async function Page({ params }: { params: { id: string } }) {
+  // Page (server) has suspense
+  // EditForm (server) loads Todo
+  // EditFormInner (client) handles form stuff
+  // This is why you get sleep
   return (
     <Suspense fallback={<p>Loading</p>}>
       <EditForm todoId={params.id} />
