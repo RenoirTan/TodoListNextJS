30a07bfccd53e7ac3924fb76778430ae2645e4b7
RenoirTan <renoirtan2005@gmail.com>
2024-01-28 22:43:42 +0800

    next-auth-0: redirect users from log in page if already logged in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,13 @@
 import LoginForm from "@/app/ui/login-form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  if (session) {
+    redirect("/");
+  }
+  
   return (
     <main>
       <LoginForm></LoginForm>
