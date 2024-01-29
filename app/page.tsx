2b09d324eda88616e7b2428ae0a633761d488bc1
RenoirTan <renoirtan2005@gmail.com>
2024-01-29 16:06:01 +0800

    next-auth-0: get name

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,19 @@ import TodosSkeleton from "@/app/ui/skeletons/todos-skeleton";
 import TodosList from "@/app/ui/todos-list";
 import LogoutButton from "@/app/ui/logout-button";
 import { auth } from "@/auth";
-import { signIn } from "next-auth/react";
+import { getUser } from "@/lib/auth";
+import { notFound } from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
-  if (!session) signIn();
+  const id = session?.user?.id;
+  if (!id) notFound();
+
+  const user = await getUser(id);
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <p>Hello, {session?.user?.name}</p>
+      <p>Hello, {user?.name}</p>
       <LogoutButton />
       <Link href="/change-password">Change Password</Link>
       <Link href="/change-name">Change Name</Link>
