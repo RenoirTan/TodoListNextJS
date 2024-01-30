3ae1627a08663995e739eecea9fc62ab51325054
RenoirTan <renoirtan2005@gmail.com>
2024-01-30 13:12:17 +0800

    styling: include nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     // from https://github.com/RenoirTan/liver/blob/main/palettes/dark.json
@@ -33,6 +35,7 @@ const config: Config = {
       "centrist": "#7f7f7f"
     }
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
