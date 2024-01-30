cc078f12fda79c4b0538d0741bca6a1bb657185f
RenoirTan <renoirtan2005@gmail.com>
2024-01-31 00:43:43 +0800

    styling: expander

--- app/ui/todo-card.tsx
+++ app/ui/todo-card.tsx
@@ -1,38 +1,81 @@
 import Link from "next/link";
 import { Todo } from "@prisma/client";
 import { editTodo as editTodoUrl } from "@/lib/urls";
-import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
-import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/16/solid";
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider } from "@nextui-org/react";
+import {
+  CheckCircleIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  XCircleIcon
+} from "@heroicons/react/16/solid";
+import { clsx } from "clsx";
+import { useState } from "react";
 
 export default function TodoCard({ todo }: { todo: Todo }) {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <div className="w-full">
-      <Link href={editTodoUrl({ id: todo.id })}>
-        <div className="border rounded-2xl border-gray">
-          <Card>
-            <CardHeader className="justify-between">
-              <h3 className="text-xl truncate">{todo.title}</h3>
-              <div className="text-sm text-neutral-100 flex flex-row gap-x-2 items-center">
-                {(todo.complete) ? (
-                  <>
-                    <p>Complete</p>
-                    <CheckCircleIcon className="h-[16px] w-[16px] text-cyan" />
-                  </>
-                ) : (
-                  <>
-                    <p>Incomplete</p>
-                    <XCircleIcon className="h-[16px] w-[16px] text-red" />
-                  </>
-                )}
-              </div>
-            </CardHeader>
-            <Divider />
-            <CardBody>
-              <p className="whitespace-pre-wrap line-clamp-6">{todo.description}</p>
-            </CardBody>
-          </Card>
-        </div>
-      </Link>
+      <div className="border rounded-2xl border-gray">
+        <Card>
+          <CardHeader className="justify-between">
+            <h3 className="text-xl truncate">{todo.title}</h3>
+            <div className="text-sm text-neutral-100 flex flex-row gap-x-2 items-center">
+              {(todo.complete) ? (
+                <>
+                  <p>Complete</p>
+                  <CheckCircleIcon className="h-[16px] w-[16px] text-cyan" />
+                </>
+              ) : (
+                <>
+                  <p>Incomplete</p>
+                  <XCircleIcon className="h-[16px] w-[16px] text-red" />
+                </>
+              )}
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p className={clsx(
+              "whitespace-pre-wrap",
+              expanded || "line-clamp-6"
+            )}>{todo.description}</p>
+          </CardBody>
+          <Divider />
+          <CardFooter className="justify-between">
+            <Link href={editTodoUrl({ id: todo.id })}>
+              <Button>
+                <p>Edit</p>
+              </Button>
+            </Link>
+            <Expander expanded={expanded} setExpanded={setExpanded} />
+          </CardFooter>
+        </Card>
+      </div>
     </div>
+  );
+}
+
+export function Expander({
+  expanded,
+  setExpanded
+}: {
+  expanded: boolean;
+  setExpanded: (expanded: boolean) => void
+}) {
+  return (
+    <Button type="button" onClick={() => setExpanded(!expanded)}>
+      {(expanded) ? (
+        <>
+          <p>Collapse</p>
+          <ChevronUpIcon className="h-[20px] w-[20px]" />
+        </>
+      ) : (
+        <>
+          <p>Expand</p>
+          <ChevronDownIcon className="h-[20px] w-[20px]" />
+        </>
+      )}
+    </Button>
   );
 }
