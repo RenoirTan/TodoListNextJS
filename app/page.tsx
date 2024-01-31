5709e6234dd3c9e864e89e2a0ae5cd9b12a7e539
RenoirTan <renoirtan2005@gmail.com>
2024-01-31 15:41:17 +0800

    main: small touch ups

--- app/page.tsx
+++ app/page.tsx
@@ -14,16 +14,16 @@ export default async function Home() {
       <div className="flex place-content-center h-full">
         <div className="mx-auto flex flex-col justify-center">
           <div className="mb-6">
-            <h1 className="text-center text-5xl font-extrabold tracking-wide mb-3 bg-clip-text text-transparent bg-gradient-to-tr from-orchid to-cyan">Todos</h1>
+            <h1 className="text-center text-5xl font-extrabold tracking-wide mb-3 bg-clip-text text-transparent bg-gradient-to-t from-orchid to-cyan">Todos</h1>
             <p className="text-center text-silver/75">Store reminders and tasks on any device.</p>
           </div>
           <div className="flex justify-center">
             <Link href={registerUrl()}>
               <Button
                 radius="full"
-                className="animate-subtle-pulse bg-gradient-to-tr from-violet to-cyan p-6"
+                className="animate-subtle-pulse bg-gradient-to-tr from-blue-violet to-cyan p-6"
               >
-                <p className="text-center text-xl font-bold text-white">Register</p>
+                <p className="text-center text-xl font-bold text-white">Try It Out Now!</p>
               </Button>
             </Link>
           </div>
