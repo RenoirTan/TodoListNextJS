2f9201ab4f3638d7c5436349830cb5a576c88e96
RenoirTan <renoirtan2005@gmail.com>
2024-01-31 10:29:49 +0800

    styling: apparently this does nothing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,32 +9,32 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    // from https://github.com/RenoirTan/liver/blob/main/palettes/dark.json
-    colors: {
-      "yellow": "#e4de41",
-      "beige": "#ffb370",
-      "orange": "#ff7f00",
-      "vermillion": "#ff6448",
-      "red": "#ff1a1a",
-      "french-fuchsia": "#fd3f92",
-      "orchid": "#f47bf0",
-      "lilac": "#eba2e8",
-      "violet": "#c390e8",
-      "blue-violet": "#9554e8",
-      "cyan": "#53e2ed",
-      "mint-green": "#4fe399",
-      "white": "#e3e3e3",
-      "silver": "#d7d7d7",
-      "dark-silver": "#9f9f9f",
-      "light-gray": "#5f5f5f",
-      "gray": "#4e4e4e",
-      "dark-gray": "#282428",
-      "black": "#1a1a1a",
-      "calm-black": "#180f18",
-      "calm-gray": "#24242e",
-      "centrist": "#7f7f7f"
-    },
     extend: {
+      // from https://github.com/RenoirTan/liver/blob/main/palettes/dark.json
+      colors: {
+        "yellow": "#e4de41",
+        "beige": "#ffb370",
+        "orange": "#ff7f00",
+        "vermillion": "#ff6448",
+        "red": "#ff1a1a",
+        "french-fuchsia": "#fd3f92",
+        "orchid": "#f47bf0",
+        "lilac": "#eba2e8",
+        "violet": "#c390e8",
+        "blue-violet": "#9554e8",
+        "cyan": "#53e2ed",
+        "mint-green": "#4fe399",
+        "white": "#e3e3e3",
+        "silver": "#d7d7d7",
+        "dark-silver": "#9f9f9f",
+        "light-gray": "#5f5f5f",
+        "gray": "#4e4e4e",
+        "dark-gray": "#282428",
+        "black": "#1a1a1a",
+        "calm-black": "#180f18",
+        "calm-gray": "#24242e",
+        "centrist": "#7f7f7f"
+      },
       animation: {
         "subtle-pulse": "subtle-pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite"
       },
