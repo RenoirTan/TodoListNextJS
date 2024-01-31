b09763cec921ef93465e7dfc9484cd724ccec489
RenoirTan <renoirtan2005@gmail.com>
2024-02-01 00:42:42 +0800

    main: images

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { register as registerUrl } from "@/lib/urls";
 import { Button } from "@nextui-org/react";
 import Navbar from "@/app/ui/navbar";
@@ -11,9 +12,29 @@ export default async function Home() {
   return (
     <main className="h-screen flex flex-col">
       <Navbar loggedIn={loggedIn} />
-      <div className="flex place-content-center h-full">
-        <div className="mx-auto flex flex-col justify-center">
-          <div className="mb-6">
+      <div className="flex place-content-center h-full max-w-4/5">
+        <div className="mx-auto flex flex-col items-center justify-center">
+
+          <div className="block md:hidden">
+            <Image
+              className="rounded-t-3xl shadow-lg shadow-gray"
+              src="/android-smushed.jpg"
+              alt="Screenshot on Android"
+              height={265}
+              width={180}
+            />
+          </div>
+          <div className="hidden md:block">
+            <Image
+              className="rounded-t-xl shadow-lg shadow-gray"
+              src="/desktop.png"
+              alt="Screenshot on Desktop"
+              height={270}
+              width={480}
+            />
+          </div>
+
+          <div className="mb-6 mt-6">
             <h1 className="text-center text-5xl font-extrabold tracking-wide mb-3 bg-clip-text text-transparent bg-gradient-to-t from-orchid to-cyan">Todos</h1>
             <p className="text-center text-silver/75">Store reminders and tasks on any device.</p>
           </div>
