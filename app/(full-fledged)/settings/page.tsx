1dc0c9679df7993536ccdbc81f05d3bb7eb141c9
RenoirTan <renoirtan2005@gmail.com>
2024-01-31 14:43:38 +0800

    styling: shadow?

--- app/(full-fledged)/settings/page.tsx
+++ app/(full-fledged)/settings/page.tsx
@@ -12,14 +12,14 @@ export default function Page() {
         <h1 className="text-center text-3xl font-bold">Settings</h1>
         <div className="flex flex-col md:flex-row mx-auto w-4/5 gap-x-12 gap-y-8">
           <SettingsButton
-            icon={<div className="p-6 rounded-lg bg-gradient-to-tr from-vermillion to-lilac">
+            icon={<div className="p-6 rounded-lg bg-gradient-to-tr from-vermillion to-lilac shadow-xl">
               <LockClosedIcon className="h-[4em] w-[4em]" />
             </div>}
             text="Change Password"
             href={changePasswordUrl()}
           />
           <SettingsButton
-            icon={<div className="p-6 rounded-lg bg-gradient-to-tr from-mint-green to-cyan">
+            icon={<div className="p-6 rounded-lg bg-gradient-to-tr from-mint-green to-cyan shadow-xl">
               <TagIcon className="h-[4em] w-[4em]" />
             </div>}
             text="Change Name"
