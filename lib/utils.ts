05f7558cc940332f6b7ca0d2628552f95e583924
RenoirTan <renoirtan2005@gmail.com>
2024-01-31 23:49:50 +0800

    main: typescript is too particular

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,14 @@
 export const COLOR_CHOICES = ["primary", "secondary", "success", "warning", "danger"];
 
 export function chooseColor(): "primary" | "secondary" | "success" | "warning" | "danger" {
-  return COLOR_CHOICES[Math.min(Math.floor(Math.random() * 5), 4)];
+  // this is necessary because typescript is very particular >:(
+  const index = Math.min(Math.floor(Math.random() * 5), 4);
+  switch (index) {
+    case 0: return "primary";
+    case 1: return "secondary";
+    case 2: return "success";
+    case 3: return "warning";
+    case 4: return "danger";
+    default: return "primary";
+  }
 }
