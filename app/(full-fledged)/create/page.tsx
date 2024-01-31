3ba035573e461468550c7a25bcc385a803e5c4e4
RenoirTan <renoirtan2005@gmail.com>
2024-01-31 11:12:55 +0800

    styling: create form and edit form now use todo form

--- app/(full-fledged)/create/page.tsx
+++ app/(full-fledged)/create/page.tsx
@@ -1,5 +1,11 @@
 import CreateForm from "@/app/ui/create-form";
 
 export default async function Page() {
-  return <CreateForm />;
+  return (
+    <div className="flex justify-center mt-8">
+      <div className="flex flex-col w-4/5 md:max-w-screen-sm">
+        <CreateForm />
+      </div>
+    </div>
+  );
 }
