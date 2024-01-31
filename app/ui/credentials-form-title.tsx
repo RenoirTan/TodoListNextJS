056924337f024680be5eb20f2bbef979afc6c557
RenoirTan <renoirtan2005@gmail.com>
2024-01-31 15:45:01 +0800

    main: credentials forms are less disgusting

--- app/ui/credentials-form-title.tsx
+++ app/ui/credentials-form-title.tsx
@@ -1,7 +1,7 @@
 export default function CredentialsFormTitle({ title }: { title: string; }) {
   return (
-    <div className="flex-1 rounded-lg bg-violet/50 px-5 pt-8 pb-4 mt-2 mb-6">
-      <h1 className="text-3xl font-bold underline decoration-solid">{title}</h1>
+    <div className="flex-1 rounded-lg bg-blue-violet/50 px-5 pt-8 pb-4 mt-2 mb-6">
+      <h1 className="text-3xl font-bold">{title}</h1>
     </div>
   );
 }
