d70d124981074401bab00e2cfdc8a175622c4e9c
RenoirTan <renoirtan2005@gmail.com>
2024-01-31 21:49:53 +0800

    main: vermillion log out button

--- app/ui/logout-button.tsx
+++ app/ui/logout-button.tsx
@@ -4,7 +4,7 @@ import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/16/solid";
 export default function LogoutButton() {
   return (
     <form action={goodbye}>
-      <button type="submit" className="flex flex-row items-center gap-x-5 md:gap-x-1.5">
+      <button type="submit" className="flex flex-row items-center gap-x-5 md:gap-x-1.5 text-vermillion">
         <ArrowLeftStartOnRectangleIcon className="h-[1em] w-[1em]" />
         <p>Log Out</p>
       </button>
