c709c8341d4628f8309f48790bad937e9a987442
RenoirTan <renoirtan2005@gmail.com>
2024-02-13 14:43:49 +0800

    oauth: button looks

--- app/ui/alternative-logins.tsx
+++ app/ui/alternative-logins.tsx
@@ -11,8 +11,12 @@ export default function AlternativeLogins() {
   const callbackUrl = searchParams?.get("callbackUrl") || todosUrl({});
 
   return (
-    <div className="flex flex-col content-center px-5 mx-10 max-w-[480px]">
-      <Button onClick={() => signIn("github", { callbackUrl })}>
+    <div className="flex flex-col content-center px-5 mx-10 max-w-[480px] gap-y-4">
+      <Button
+        className="dark:shadow-lg dark:shadow-green-900"
+        color="success"
+        onClick={() => signIn("github", { callbackUrl })}
+      >
         <span className="flex flex-row items-center text-center gap-x-3">
           <FaGithub className="h-[1.5em] w-[1.5em]" />
           Log in with GitHub
