e6f4de75a142a22c57d63a7759a3e6bbf805e80e
RenoirTan <renoirtan2005@gmail.com>
2024-02-13 11:24:04 +0800

    oauth: use nextauth's default login flow while i figure everything out

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,7 @@ import { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
   pages: {
-    signIn: "/login",
+    // signIn: "/login",
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
