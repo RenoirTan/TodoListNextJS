7e92f07689ed00ae5d8205bdaa84e4dc1baf5a43
RenoirTan <renoirtan2005@gmail.com>
2024-02-13 14:41:26 +0800

    oauth: react-icons

--- app/ui/alternative-logins.tsx
+++ app/ui/alternative-logins.tsx
@@ -4,6 +4,7 @@ import { todos as todosUrl } from "@/lib/urls";
 import { useSearchParams } from "next/navigation";
 import { Button } from "@nextui-org/react";
 import { signIn } from "next-auth/react";
+import { FaGithub } from "react-icons/fa";
 
 export default function AlternativeLogins() {
   const searchParams = useSearchParams();
@@ -12,7 +13,10 @@ export default function AlternativeLogins() {
   return (
     <div className="flex flex-col content-center px-5 mx-10 max-w-[480px]">
       <Button onClick={() => signIn("github", { callbackUrl })}>
-        Log in with GitHub
+        <span className="flex flex-row items-center text-center gap-x-3">
+          <FaGithub className="h-[1.5em] w-[1.5em]" />
+          Log in with GitHub
+        </span>
       </Button>
     </div>
   )
